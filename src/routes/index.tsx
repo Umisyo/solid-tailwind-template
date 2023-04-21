f634c7f9d89ecc9fc50b099d0538cc33d19c2b5b
Umisyo <s_kusunoki@yumemi.co.jp>
2023-04-21 16:14:26 +0900

    feat: add tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,10 @@
 import { Title } from 'solid-start'
-import Counter from '~/components/Counter'
 
 export default function Home() {
   return (
-    <main>
+    <main class="w-full">
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{' '}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{' '}
-        to learn how to build SolidStart apps.
-      </p>
+      <h1 class="text-red-500 text-center">Hello world!</h1>
     </main>
   )
 }
